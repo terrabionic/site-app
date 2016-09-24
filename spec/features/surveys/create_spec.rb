b4cbccea987dc1500892a8da7fa83e3ecb908c25
imontalvomiguel <ubaldo.im@gmail.com>
2016-09-24 13:11:43 -0500

    refactor survey creation in spec

--- spec/features/surveys/create_spec.rb
+++ spec/features/surveys/create_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 describe "Creating surveys" do
-  it "redirects to the survey show page on success" do
+  def create_survey(options = {})
+    options[:title] ||= "Default survey title"
+
     visit surveys_path
     click_link "New Survey"
-    expect(page).to have_content("New Survey")
 
-    fill_in "Title", with: "Healthy Enterprise"
+    fill_in "Title", with: options[:title]
     click_button "Create Survey"
+  end
+
+  it "redirects to the survey show page on success" do
+
+    create_survey(title: "Healthy Enterprise")
 
     expect(page).to have_content("Healthy Enterprise")
 
   end
 
   it "displays an error when the survey has no title" do
+
     expect(Survey.count).to eq(0)
 
-    visit surveys_path
-
-    click_link "New Survey"
-    expect(page).to have_content("New Survey")
-
-    fill_in "Title", with: ""
-    click_button "Create Survey"
+    create_survey(title: "")
 
     expect(page).to have_content("error")
     expect(Survey.count).to eq(0)
