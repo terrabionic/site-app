b0382edccd1edd02a6d7dc78c0676515b48ad587
imontalvomiguel <ubaldo.im@gmail.com>
2016-09-25 16:35:30 -0500

    associate questions with notes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   belongs_to :category
+  has_many :notes, :dependent => :restrict_with_error
 
   validates :title, presence: true
   validates :category, presence: true
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should belong_to(:survey) }
   it { should belong_to(:category) }
+  it { should have_many(:notes) }
 end
