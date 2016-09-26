44c41e9f34ba573a7122c5cfcaee2ef0c7629361
imontalvomiguel <ubaldo.im@gmail.com>
2016-09-26 02:01:03 -0500

    move validators to initializers directory

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,12 +1,3 @@
-#todo place this class into its own file
-class LteGradeValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if value && value > record.question.grade
-      record.errors[attribute] << (options[:message] || "can't be greater than question grade")
-    end
-  end
-end
-
 class Note < ActiveRecord::Base
   belongs_to :question
 
