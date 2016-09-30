62dc263fdfb90644d9375744bd43b1220ce01ada
imontalvomiguel <ubaldo.im@gmail.com>
2016-09-30 15:14:00 -0500

    tests and code to edit replies

--- spec/features/replies/edit_spec.rb
+++ spec/features/replies/edit_spec.rb
@@ -6,6 +6,12 @@ describe "Editing replies" do
 
     survey = FactoryGirl.create(:full_survey, questions_count: 1, replies_count: 1)
 
+    first_possible_answer = survey.questions.first.possible_answers.first
+    last_possible_answer = survey.questions.first.possible_answers.last
+
+    first_possible_answer.update(title: 'Bad')
+    last_possible_answer.update(title: 'Good')
+
     visit surveys_path
 
     within "#survey_#{survey.id}" do
@@ -16,15 +22,19 @@ describe "Editing replies" do
 
     expect(page).to have_content("New Reply")
 
-    survey.questions.each_with_index do |question, index|
-      fill_in question.title, with: "Arch Linux"
-    end
+    choose('Good')
 
     click_button "Create Reply"
 
     expect(page).to have_content "success"
 
     expect(survey.replies.last.answers.count).to eq(1)
+
+    expect(survey.replies.count).to eq(2)
+
+    first_answer =survey.replies.last.answers.first
+
+    expect(first_answer.possible_answer_id).to eq(last_possible_answer.id)
 
     visit surveys_path
 
@@ -36,19 +46,19 @@ describe "Editing replies" do
       click_link "Edit"
     end
 
-    survey.replies.last.answers.each do |answer|
-      fill_in answer.question.title, with: "Ubuntu"
-    end
+    choose('Bad')
 
     click_button "Update Reply"
 
     expect(page).to have_content("success")
 
-    expect(survey.replies.last.answers.first.content).to eq("Ubuntu")
-
     expect(survey.replies.last.answers.count).to eq(1)
 
     expect(survey.replies.count).to eq(2)
+
+    first_answer =survey.replies.last.answers.first
+
+    expect(first_answer.possible_answer_id).to eq(first_possible_answer.id)
 
   end
 
