09f3874ab6acd879006c2e63f70d4cd9006a78d6
imontalvomiguel <ubaldo.im@gmail.com>
2016-09-30 21:09:20 -0500

    back to the survey questions

--- spec/features/possible_answers/index_spec.rb
+++ spec/features/possible_answers/index_spec.rb
@@ -23,4 +23,26 @@ describe "Viewing possible answers" do
 
   end
 
+  it "redirects to survey questions by clicking the back button" do
+    survey = FactoryGirl.create(:full_survey, questions_count: 2)
+
+    visit surveys_path
+
+    within "#survey_#{survey.id}" do
+      click_link "Questions"
+    end
+
+    question = survey.questions.first
+
+    within "#question_#{question.id}" do
+      click_link "Possible answers"
+    end
+
+    click_link "Back"
+
+    expect(page).to have_content("Listing Questions")
+    expect(page).to have_content(survey.title)
+
+  end
+
 end
