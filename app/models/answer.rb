4394ee166a98009c0a9c0c105e08a966fd6fbea9
imontalvomiguel <ubaldo.im@gmail.com>
2016-10-03 01:42:42 -0500

    don't show grade nor title when possible answer is not set

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,11 +3,11 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   def title
-    self.question.possible_answers.find(self.possible_answer_id).title
+    self.question.possible_answers.find(self.possible_answer_id).title if self.possible_answer_id
   end
 
   def grade
-    self.question.possible_answers.find(self.possible_answer_id).grade
+    self.question.possible_answers.find(self.possible_answer_id).grade if self.possible_answer_id
   end
 
 end
--- spec/features/replies/show_spec.rb
+++ spec/features/replies/show_spec.rb
@@ -10,6 +10,29 @@ describe "Showing a reply" do
     end
   end
 
+  it "doest not display grade nor answer title when a possible answer has not filled through the reply" do
+
+    visit_survey_replies(survey)
+
+    click_link "New Reply"
+
+    click_button "Create Reply"
+
+    visit_survey_replies(survey)
+
+    last_reply = survey.replies.last
+
+    within "#reply_#{last_reply.id}" do
+      click_link "Show"
+    end
+
+    expect(last_reply.answers.first.possible_answer_id).to eq(nil)
+    expect(last_reply.answers.last.possible_answer_id).to eq(nil)
+
+    expect(page).to_not have_content "error"
+
+  end
+
   it "displays question/answer and its grade" do
 
     first_possible_answer = survey.questions.first.possible_answers.first
