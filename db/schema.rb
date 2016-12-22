4a3734105c35f006476dfcc660d4db0703a04ec8
Carlos <carlos.blanco@grupoaltegra.com>
2016-12-22 13:15:47 -0600

    [ADD] Added field to companies order and added print date pdf

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216000544) do
+ActiveRecord::Schema.define(version: 20161222185814) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "reply_id"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(version: 20161216000544) do
     t.integer  "num_electronic_kiosk"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.date     "date_print"
   end
 
   create_table "surveys", force: :cascade do |t|
