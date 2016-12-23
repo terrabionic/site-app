9a2bcb7031a2b27534215267d9094a3040d13aa5
daimon <daimon@debian7>
2016-12-23 14:02:02 -0600

    [REF] Now you can send contact mail

--- app/mailers/notification_site.rb
+++ app/mailers/notification_site.rb
@@ -15,4 +15,16 @@ class NotificationSite < ApplicationMailer
     @url  = 'http://162.243.75.86:3000'
     mail to: @user.email, subject: "Bienvenido a Site"
   end
+  
+  def notify_contact(contact)
+  	@contact = contact
+    @login_manager = LoginManager.first
+    unless @login_manager
+		@login_manager = LoginManager.create()
+    end
+    #@url  = 'http://Site.com'
+    @url  = 'http://162.243.75.86:3000'
+    mail to: @login_manager.email_contact, subject: @contact.subject
+  end
+  
 end
--- app/models/login_manager.rb
+++ app/models/login_manager.rb
@@ -1,2 +1,3 @@
 class LoginManager < ActiveRecord::Base
+	validates :email_contact, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 end
